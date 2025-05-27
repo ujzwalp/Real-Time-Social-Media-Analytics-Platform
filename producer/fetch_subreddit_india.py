2244a14669df4b3a92d73529986867a9d5b6ef28
ujzwalp <ujjwalpandey256@gmail.com>
2025-05-28 00:39:18 +0530

    producing top and hot reddit submissions

--- producer/fetch_subreddit_india.py
+++ producer/fetch_subreddit_india.py
@@ -1,34 +1,132 @@
-import praw, os, re
+import praw, os, json, pprint
 from dotenv import load_dotenv
 load_dotenv()
 
-class Subreddit_india:
-    def __init__(self):
-        pass
-    
-    
-    def create_reddit_instance(self):
-        """
-            Implemented in the sub-class.
-        """
-        
-    
-        
-class Get_India_Feed(Subreddit_india):
+class Subreddit_India:
     def __init__(self):
         self._user_agent = "script:producer_reddit_api:v1.0 (by /u/ujzwalp1710)"
         self._client_id = os.getenv("client_id")
         self._client_secret = os.getenv("client_secret")
-            
-    def create_reddit_instance(self):
-        reddit = praw.Reddit(
+      
+    def get_hot_submissions(self):
+        raise ValueError("Not a valid request for hot submissions! Implemented in subclass")
+    
+    def get_best_submissions(self):
+        raise ValueError("Not a valid request for hot submissions! Implemented in subclass")
+    
+        
+class Get_India_Feed(Subreddit_India):
+    def __init__(self):
+        self._user_agent = "script:producer_reddit_api:v1.0 (by /u/ujzwalp1710)"
+        self._client_id = os.getenv("client_id")
+        self._client_secret = os.getenv("client_secret")
+        
+        self.reddit = praw.Reddit(
             client_id = self._client_id,
             client_secret = self._client_secret,
             refresh_token = os.getenv("refresh_token"),
             user_agent = self._user_agent
         )
         
-        print("Script granted access to the api with the following privilege: ", reddit.auth.scopes())
+        print("Script granted access to the api with the following privilege: ",   self.reddit.auth.scopes())  
         
-indian_feed_obj = Get_India_Feed()
-indian_feed_obj.create_reddit_instance()
+        
+    def get_hot_submissions(self):
+        subreddit = self.reddit.subreddit("India")
+        submissions = subreddit.hot(limit=500)
+        hot_submission_list = []
+        
+        
+        for submission in submissions:
+            message_dict = {
+                "id": submission.id,
+                "title": submission.title,
+                "content": submission.selftext,
+                "score": submission.score,
+                "likes": submission.likes,
+                "ups": submission.ups,
+                "downs": submission.downs,
+                "upvote_ratio": submission.upvote_ratio,
+                "total_comments": submission.num_comments,
+                "edited": submission.edited,
+                "is_video": submission.is_video,
+                "is_original_content": submission.is_original_content,
+                "self_post": submission.is_self,
+                "media": submission.media,
+                "media_embed": submission.media_embed,
+                "media_only": submission.media_only,
+                "tags":submission.link_flair_text,
+                "category": submission.category,
+                "content_category": submission.content_categories,
+                "discussion_type": submission.discussion_type,
+                "over_18": submission.over_18,
+                "domain": submission.domain,
+                "total_awards": submission.total_awards_received,
+                "awards": submission.all_awardings,
+                "author": submission.author.name if submission.author else "[deleted]",
+                "author_id": getattr(submission.author, "id", "t2_NA"),
+                "post_date": submission.created_utc,
+                "post_url": submission.url,
+                "post_premalink": submission.permalink,
+                "subreddit_name": submission.subreddit_name_prefixed,
+                "subreddit_id": submission.subreddit_id,
+                "subreddit_subscribers": submission.subreddit_subscribers,
+            }
+            
+            message = json.dumps(message_dict)
+            
+            hot_submission_list.append(message)
+            
+        return hot_submission_list
+    
+    def get_top_submissions(self):
+        subreddit = self.reddit.subreddit("India")
+        submissions = subreddit.top(time_filter="all", limit=500)
+        top_submission_list = []
+        
+        
+        for submission in submissions:
+            message_dict = {
+                "id": submission.id,
+                "title": submission.title,
+                "content": submission.selftext,
+                "score": submission.score,
+                "likes": submission.likes,
+                "ups": submission.ups,
+                "downs": submission.downs,
+                "upvote_ratio": submission.upvote_ratio,
+                "total_comments": submission.num_comments,
+                "edited": submission.edited,
+                "is_video": submission.is_video,
+                "is_original_content": submission.is_original_content,
+                "self_post": submission.is_self,
+                "media": submission.media,
+                "media_embed": submission.media_embed,
+                "media_only": submission.media_only,
+                "tags":submission.link_flair_text,
+                "category": submission.category,
+                "content_category": submission.content_categories,
+                "discussion_type": submission.discussion_type,
+                "over_18": submission.over_18,
+                "domain": submission.domain,
+                "total_awards": submission.total_awards_received,
+                "awards": submission.all_awardings,
+                "author": submission.author.name if submission.author else "[deleted]",
+                "author_id": getattr(submission.author, "id", "t2_NA"),
+                "post_date": submission.created_utc,
+                "post_url": submission.url,
+                "post_premalink": submission.permalink,
+                "subreddit_name": submission.subreddit_name_prefixed,
+                "subreddit_id": submission.subreddit_id,
+                "subreddit_subscribers": submission.subreddit_subscribers,
+            }
+            
+            message = json.dumps(message_dict)
+            
+            top_submission_list.append(message)
+            
+        return top_submission_list
+    
+obj_india_feed = Get_India_Feed()
+list1 = obj_india_feed.get_top_submissions()
+pprint.pprint(list1)
